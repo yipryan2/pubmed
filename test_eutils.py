7ffa1b75798c0fe53493c8d7fee5a0b416c33e36
gui11aume <guillaume.filion@gmail.com>
2021-05-02 01:26:21 -0400

    Remove fixture and clean calls to test functions

--- test_eutils.py
+++ test_eutils.py
@@ -8,19 +8,6 @@ import tempfile
 import time
 
 
-@pytest.fixture
-def eutils_options():
-    # make sure we do not over the 3 requests per second limit
-    time.sleep(1)
-    eutils_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
-    esearch_param = 'esearch.fcgi?db=pubmed&term=hiv+or+aids&usehistory=y&retmode=json'
-    return {
-        'esearch_expected_url': eutils_url + esearch_param,
-        'error_url': 'https://www.google.com/nothere',
-        'query': "hiv or aids",
-    }
-
-
 def test_esearch():
     # Query one article.
     result = eutils.esearch('27870832[pmid]')
@@ -46,7 +33,7 @@ def test_esearch_error():
         assert ex.response.status_code == 404
 
 
-def test_efetch(eutils_options):
+def test_efetch():
     _, _, querykey, webenv = eutils.esearch('HIV')
     with tempfile.TemporaryFile('w+') as tmpf:
         result = eutils.efetch(20, querykey, webenv, retmax=10, outfile=tmpf)
@@ -57,7 +44,7 @@ def test_efetch(eutils_options):
     assert result[0] + result[2] == 20
 
 
-def test_efetch_error(eutils_options):
+def test_efetch_error():
     # Test that a query to the wrong URL raises an error 404.
     with pytest.raises(requests.exceptions.HTTPError) as ex:
         _ = eutils.efetch(10, '1', 'MCID_608d46e325a8b65cf511b0e5',
